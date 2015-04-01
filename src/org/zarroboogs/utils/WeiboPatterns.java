4f61b0d4a35405260ac606ac2c7d691fe524621e
andforce <86118@163.com>
2015-04-01 23:30:59 +0800

    refactor code

--- src/org/zarroboogs/utils/WeiboPatterns.java
+++ src/org/zarroboogs/utils/WeiboPatterns.java
@@ -3,9 +3,6 @@ package org.zarroboogs.utils;
 
 import java.util.regex.Pattern;
 
-/**
- * User: qii Date: 12-12-27 fix web url bug which include chinese words, see WEB_URL
- */
 public class WeiboPatterns {
 
     public static final Pattern WEB_URL = Pattern.compile("http://[a-zA-Z0-9+&@#/%?=~_\\-|!:,\\.;]*[a-zA-Z0-9+&@#/%=~_|]");
