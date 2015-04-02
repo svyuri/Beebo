0f1efd562b3edcd714f67d33a4f9550240f985ae
andforce <86118@163.com>
2015-04-02 13:32:04 +0800

    refactor code

--- src/org/zarroboogs/utils/WeiBoURLs.java
+++ src/org/zarroboogs/utils/WeiBoURLs.java
@@ -263,29 +263,56 @@ public class WeiBoURLs {
 	}
     public static String hotHuaTiOneHouOur(String gsid, int page){
     	
-    	String url = "http://api.weibo.cn/2/cardlist?uicode=10000011&lcardid=uve_popular_topics&c=android&i=8764dac&s=b48eb5c9&ua=smartisan-SM701__weibo__5.1.1__android__android4.4.2&wm=14010_0013&fid=100803&uid=1878230075&v_f=2&v_p=18&from=1051195010&"
-    			+ "gsid=" + gsid
-    			+ "&imsi=460020364311738&lang=zh_CN&lfid=1001000002_1582&"
-    			+ "page="+ page
-    			+ "&skin=default&count=20&oldwm=14010_0013&containerid=100803&luicode=10000010";
-    	return url;
+    	return SeniorUrl.hotHuaTiApi(gsid, "-1", page);
     }
     
     public static String hotModel(String gsid, int page){
     	String url = "http://mapi.weibo.com/2/cardlist?"
     			+ "gsid=" + gsid
-    			+ "&wm=3333_2001&i=d5d6f09&b=1&from=1051293010&c=iphone&v_p=18&skin=default&v_f=1&s=a57eef07&lang=zh_CN&ua=iPhone6,1__weibo__5.1.2__iphone__os8.1.2&uicode=10000011&luicode=10000011&count=20&containerid=101903_-_girl_pic_lst&fid=101903_-_girl_pic_lst&lfid=101903&"
-    			+ "page=" + page;
+    			+ "&wm=3333_2001"
+    			+ "&i=d5d6f09"
+    			+ "&b=1"
+    			+ "&from=1051293010"
+    			+ "&c=iphone"
+    			+ "&v_p=18"
+    			+ "&skin=default"
+    			+ "&v_f=1"
+    			+ "&s=a57eef07"
+    			+ "&lang=zh_CN"
+    			+ "&ua=iPhone6,1__weibo__5.1.2__iphone__os8.1.2"
+    			+ "&uicode=10000011"
+    			+ "&luicode=10000011"
+    			+ "&count=20"
+    			+ "&containerid=101903_-_girl_pic_lst"
+    			+ "&fid=101903_-_girl_pic_lst"
+    			+ "&lfid=101903"
+    			+ "&page=" + page;
     	return url;
     }
     
     public static String hotModelDetail(String gsid, int page, String extparam) {
 		String url = "http://api.weibo.cn/2/cardlist?"
 				+ "gsid=" + gsid
-				+ "&wm=3333_2001&i=d5d6f09&b=1&from=1051293010&c=iphone&v_p=18&skin=default&v_f=1&s=a57eef07&lang=zh_CN&ua=iPhone6,1__weibo__5.1.2__iphone__os8.1.2&uicode=10000011&luicode=10000011&count=20&"
-				+ "extparam=" + extparam
-				+ "&containerid=101903_-_girl_pic_dtl&fid=101903_-_girl_pic_dtl&lfid=101903_-_girl_pic_lst&sourcetype=page&"
-				+ "page=" + page;
+				+ "&wm=3333_2001"
+				+ "&i=d5d6f09"
+				+ "&b=1"
+				+ "&from=1051293010"
+				+ "&c=iphone"
+				+ "&v_p=18"
+				+ "&skin=default"
+				+ "&v_f=1"
+				+ "&s=a57eef07"
+				+ "&lang=zh_CN"
+				+ "&ua=iPhone6,1__weibo__5.1.2__iphone__os8.1.2"
+				+ "&uicode=10000011"
+				+ "&luicode=10000011"
+				+ "&count=20"
+				+ "&extparam=" + extparam
+				+ "&containerid=101903_-_girl_pic_dtl"
+				+ "&fid=101903_-_girl_pic_dtl"
+				+ "&lfid=101903_-_girl_pic_lst"
+				+ "&sourcetype=page"
+				+ "&page=" + page;
 		return url;
 	}
     
