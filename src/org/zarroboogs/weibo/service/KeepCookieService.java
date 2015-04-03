0944590c4b1d96fe921e7318003a5e574bf487d3
andforce <86118@163.com>
2015-04-03 13:05:43 +0800

    add KeepCookieService

---
+++ src/org/zarroboogs/weibo/service/KeepCookieService.java
@@ -0,0 +1,15 @@
+package org.zarroboogs.weibo.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class KeepCookieService extends Service {
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
