12b4fcf1cf5035acf1156c91eaefd84fa4ab34c6
andforce <86118@163.com>
2015-04-26 18:48:19 +0800

    add root

--- app/src/main/java/org/zarroboogs/utils/RootUtils.java
+++ app/src/main/java/org/zarroboogs/utils/RootUtils.java
@@ -4,11 +4,69 @@ import android.util.Log;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 
 /**
  * Created by andforce on 15-4-25.
  */
 public class RootUtils {
+
+    private static final String TAG = "RootUtils";
+
+    // 执行命令并且输出结果
+    public static String execRootCmd(String cmd) {
+        String result = "";
+        DataOutputStream dos = null;
+        DataInputStream dis = null;
+        Process p = null;
+        try {
+            p = Runtime.getRuntime().exec("su");// 经过Root处理的android系统即有su命令
+            dos = new DataOutputStream(p.getOutputStream());
+            dis = new DataInputStream(p.getInputStream());
+
+            Log.i(TAG, cmd);
+            dos.writeBytes(cmd + "\n");
+            // dos.flush();
+            dos.writeBytes("exit\n");
+            dos.flush();
+            String line = null;
+            while ((line = dis.readLine()) != null) {
+                // Log.d("result", line);
+                if (result == "") {
+                    result += line;
+                } else {
+                    result += "\n" + line;
+                }
+            }
+            p.waitFor();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (dos != null) {
+                try {
+                    dos.close();
+                    dos = null;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (dis != null) {
+                try {
+                    dis.close();
+                    dis = null;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (p != null) {
+                p.destroy();
+                p = null;
+            }
+        }
+        return result;
+    }
+
+
     public static boolean canRunRootCommands() {
         boolean retval = false;
         Process suProcess;
@@ -54,6 +112,7 @@ public class RootUtils {
             Log.d("ROOT", "Root access rejected [" + e.getClass().getName() + "] : " + e.getMessage());
         }
 
+        Log.d("ROOT", "Root access granted Has Rooted: " + retval);
         return retval;
     }
 
--- app/src/main/java/org/zarroboogs/weibo/setting/fragment/SettingsFragment.java
+++ app/src/main/java/org/zarroboogs/weibo/setting/fragment/SettingsFragment.java
@@ -1,18 +1,43 @@
 package org.zarroboogs.weibo.setting.fragment;
 
+import org.zarroboogs.devutils.DevLog;
+import org.zarroboogs.utils.CommUtils;
 import org.zarroboogs.utils.Constants;
+import org.zarroboogs.utils.RootUtils;
+import org.zarroboogs.weibo.BeeboApplication;
 import org.zarroboogs.weibo.R;
 import org.zarroboogs.weibo.activity.AccountActivity;
+import org.zarroboogs.weibo.db.AccountDatabaseManager;
+import org.zarroboogs.weibo.db.table.AccountTable;
 import org.zarroboogs.weibo.setting.activity.SettingActivity;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Looper;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceFragment;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SettingsFragment extends PreferenceFragment {
 
+    private Preference mRootPreference;
+
+    private ProgressDialog progressDialog;
+
+    private RootAsyncTask rootTask;
+
+
+    private static final String TAG = "SettingsFragment";
+
+    private static final String SINA_PREF_PATH = "/data/data/com.sina.weibo/shared_prefs/sina_push_pref.xml";
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -21,6 +46,30 @@ public class SettingsFragment extends PreferenceFragment {
 		} else {
 			addPreferencesFromResource(R.xml.setting_activity_pref);
 		}
+
+        mRootPreference = findPreference("root_request_key");
+        mRootPreference.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle("高级授权");
+                builder.setMessage("如果有任何疑问请不要使用该功能，点击确定开始授权，点击取消不进行任何操作！")
+                        .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                                if (CommUtils.isInstalled(getActivity(), "com.sina.weibo")) {
+                                    new RootAsyncTask().execute();
+
+                                } else {
+                                    showSinaWeiboNotInstalledDialog();
+                                }
+
+                            }
+                        }).setNegativeButton("取消",null).create().show();
+                return false;
+            }
+        });
 
 		Preference myPref = (Preference) findPreference(SettingActivity.CHANGE_WEIBO_ACCOUNT);
 		myPref.setOnPreferenceClickListener(new OnPreferenceClickListener() {
@@ -34,6 +83,123 @@ public class SettingsFragment extends PreferenceFragment {
 
 		});
 	}
+
+    private void showProgressDialog(){
+        Looper.prepare();
+        if (progressDialog == null){
+            progressDialog = new ProgressDialog(getActivity());
+            progressDialog.setMessage("正在获取高级授权...");
+        }
+
+        progressDialog.show();
+
+        Thread t = getGsidUid();
+        t.start();
+
+
+        Looper.loop();
+    }
+
+    private Thread getGsidUid() {
+
+
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+
+                String gsid = "";
+                String uid = "";
+
+                String cmd = "cat " + SINA_PREF_PATH;
+                DevLog.printLog(TAG + "_ROOT_CMD", cmd);
+                String result = RootUtils.execRootCmd(cmd);
+
+                String GSID_P = "<string name=\"key.gsid\">.*";
+                Pattern p = Pattern.compile(GSID_P);
+                Matcher m = p.matcher(result);
+                if (m.find()){
+                    gsid = result.substring(m.start(), m.end()).replace("</string>","").replace("<string name=\"key.gsid\">","");
+                    DevLog.printLog(TAG + "_ROOT_CMD", gsid);
+                }
+
+                String UID_P = "<long name=\"key.uid.new\" value=.*";
+                Pattern uidPattern = Pattern.compile(UID_P);
+                Matcher uidMatcher = uidPattern.matcher(result);
+                if (uidMatcher.find()){
+                    uid = result.substring(uidMatcher.start(), uidMatcher.end()).replace("\" />","").replace("<long name=\"key.uid.new\" value=\"","");
+                    DevLog.printLog(TAG + "_ROOT_CMD", uid);
+                }
+
+                String accountUID = BeeboApplication.getInstance().getAccountBean().getUid();
+                if (!accountUID.equals(uid)){
+                    showAuthFailed();
+
+                }else {
+                    AccountDatabaseManager manager = new AccountDatabaseManager(getActivity().getApplicationContext());
+                    manager.updateAccount(AccountTable.ACCOUNT_TABLE, uid, AccountTable.GSID, accountUID);
+                    showAuthSuccess();
+                }
+
+                rootTask.cancel(true);
+            }
+        };
+        return new Thread(r);
+    }
+
+    private void showAuthSuccess(){
+        Looper.prepare();
+        progressDialog.dismiss();
+        Toast.makeText(getActivity().getApplicationContext(),"授权成功!!", Toast.LENGTH_LONG).show();
+        Looper.loop();
+    }
+
+    private void showAuthFailed(){
+        Looper.prepare();
+        progressDialog.dismiss();
+        Toast.makeText(getActivity().getApplicationContext(),"当前登录帐号和官方客户端登录的帐号不一致!", Toast.LENGTH_LONG).show();
+        Looper.loop();
+    }
+    private void dissmissProgressDialog(){
+
+        Looper.prepare();
+        if (progressDialog == null || !progressDialog.isShowing()){
+            return;
+        }
+        Toast.makeText(getActivity().getApplicationContext(), "的手机没有Root或者你不信任iBeebo",Toast.LENGTH_LONG).show();
+        progressDialog.dismiss();
+        Looper.loop();
+    }
+
+    private void showSinaWeiboNotInstalledDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("错误").setMessage("你没有安装官方客户端，但这不是广告，只是为了获取官方的高级授权，授权完成之后可以卸载官方客户端，请安装并登录")
+                .setPositiveButton("确定", null).create().show();
+    }
+
+//_2A254ONmkDeTxGeRM4lYQ9C_JwjiIHXVZbGpsrDV6PUJbrdAKLU3tkWqQw68bwe8plnmT60PLmoKDzKF7lA..
+
+    class RootAsyncTask extends AsyncTask<Void, Void, Void> {
+
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            this.cancel(true);
+        }
+
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            if (RootUtils.canRunRootCommands()){
+                showProgressDialog();
+            }else {
+                dissmissProgressDialog();
+
+            }
+            return null;
+        }
+    }
+
 
 	private void showAccountSwitchPage() {
 		Intent intent = AccountActivity.newIntent();
